f234233bd938877bf1743ccc9d046a646a4f67d9
DESKTOP-0ME1KU2\Juan Hernandez <juanpa3624@gmail.com>
2018-03-09 20:52:24 -0500

    First commit of application something Interfaces, navigation in the windows and begin of Call of Camera

---
+++ app/src/main/java/com/example/juanhernandez/project/Activities/pantalla_registro.java
@@ -0,0 +1,21 @@
+package com.example.juanhernandez.project.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.juanhernandez.project.R;
+
+/**
+ * Created by Juan Hernandez on 5/03/2018.
+ */
+
+public class pantalla_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_registro);
+        {
+        }
+    }
+}
---
+++ app/src/main/java/com/example/juanhernandez/project/Activities/pantalla_usuario.java
@@ -0,0 +1,20 @@
+package com.example.juanhernandez.project.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.juanhernandez.project.R;
+
+/**
+ * Created by Juan Hernandez on 5/03/2018.
+ */
+
+public class pantalla_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_usuario);
+
+    }
+}
