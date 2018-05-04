55944521a90e1e5ae3e80c7b460b8cc41a9efacb
Daniel Rivera <daniele.riverag@konradlorenz.edu.co>
2018-05-03 23:16:14 -0500

    the registration and login views are generated by using firebases, the Users class is generated in order to capture the user's data during the registration, sources are added and the application is given a better appearance

---
+++ app/src/main/java/com/example/juanhernandez/project/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.juanhernandez.project;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.juanhernandez.project.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    TextView txtSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnsignIn);
+        btnSignUp = (Button)findViewById(R.id.btnsignUp);
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/NABILA.TTF");
+        txtSlogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SignIn.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
